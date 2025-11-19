0ce3585614bebb0a4a26e5cfe79792278b20c0bb
123chen <15088816869@163.com>
2025-11-19 16:02:13 +0800

    1

--- vuepress.config.ts
+++ vuepress.config.ts
@@ -2,7 +2,6 @@ import { vipDocSite, VipPackageJSON } from '@142vip/utils'
 import {
   defineVipVuepressConfig,
   getVipHopeTheme,
-  handleImportCodePath,
   VUEPRESS_DEFAULT_DOCS_DIR,
 } from '@142vip/vuepress'
 import { navbarConfig, sidebarConfig } from './docs/theme.config'
@@ -15,13 +14,6 @@ export default defineVipVuepressConfig({
   port: 4200,
   head: [['link', { rel: 'icon', href: 'favicon.ico' }]],
   markdown: {
-    importCode: {
-      handleImportPath: handleImportCodePath([
-        ['@code', 'code'],
-        ['@ds', 'code/ds'],
-        ['~', ''],
-      ]),
-    },
     headers: { level: [2, 3, 4] },
   },
   theme: getVipHopeTheme({
@@ -34,19 +26,10 @@ export default defineVipVuepressConfig({
     },
     logo: '/images/logo.jpeg',
     logoDark: '/images/logo.jpeg',
-    hostname: 'localhost',
-    footer: `<div>All Rights Reserved | <strong>${pkg.name}@v${pkg.version}</strong></div>`,
-    copyright: `版权所有 © 2015-${new Date().getFullYear()} 408CSFamily`,
-    author: { name: '408CSFamily' },
     docsDir: VUEPRESS_DEFAULT_DOCS_DIR,
-    docsBranch: 'main',
-    docsRepo: `https://github.com/142vip/${pkg.name}`,
-    contributors: true,
     plugins: {
       search: {
         maxSuggestions: 10,
-        isSearchable: page => page.path !== '/',
-        getExtraFields: page => (page.frontmatter.title ? [page.frontmatter.title as string] : []),
         locales: {
           '/': {
             placeholder: '搜索文档',
@@ -55,7 +38,6 @@ export default defineVipVuepressConfig({
           },
         },
       },
-      watermark: { enabled: false },
     },
   }),
 })
